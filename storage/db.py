dbbde519d7b704c0428c73be2420f73eb02434b7
ygbirdz-oss <ygbirdz@gmail.com>
2026-04-06 03:01:05 +0300

    Update db.py

--- storage/db.py
+++ storage/db.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+DB_PATH = "bot.db"
+
+
+def get_conn():
+    return sqlite3.connect(DB_PATH)
+
+
+def init_db():
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS bad_words (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        word TEXT UNIQUE
+    )
+    """)
+
+    conn.commit()
+    conn.close()
